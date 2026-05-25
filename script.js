8bc0bbf6b445bb09dde8ad17a1d544dd27e27142
fercupdv <fercup87@gmail.com>
2026-05-25 00:58:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,30 +4,32 @@ const lightbox = document.getElementById('lightbox');
 const imagenLightbox = document.getElementById('imagen-lightbox');
 const botonCerrar = document.getElementById('cerrar-lightbox');
 
-imagenesGaleria.forEach(imagen => {
-    imagen.addEventListener('click', () => {
-        
-        imagenLightbox.src = imagen.src;
-        
-        lightbox.classList.remove('lightbox-oculto');
-    });
-});
-
-
-botonCerrar.addEventListener('click', () => {
-    lightbox.classList.add('lightbox-oculto');
-});
-
-
-lightbox.addEventListener('click', (evento) => {
-    if (evento.target === lightbox) {
-        lightbox.classList.add('lightbox-oculto');
-    }
-});
-
 const btnMenu = document.getElementById('btn-menu');
 const menuEnlaces = document.getElementById('menu-enlaces');
 
-btnMenu.addEventListener('click', () => {
-    menuEnlaces.classList.toggle('mostrar');
-});
+
+if (lightbox && botonCerrar) {
+    imagenesGaleria.forEach(imagen => {
+        imagen.addEventListener('click', () => {
+            imagenLightbox.src = imagen.src;
+            lightbox.classList.remove('lightbox-oculto');
+        });
+    });
+
+    botonCerrar.addEventListener('click', () => {
+        lightbox.classList.add('lightbox-oculto');
+    });
+
+    lightbox.addEventListener('click', (evento) => {
+        if (evento.target === lightbox) {
+            lightbox.classList.add('lightbox-oculto');
+        }
+    });
+}
+
+
+if (btnMenu && menuEnlaces) {
+    btnMenu.addEventListener('click', () => {
+        menuEnlaces.classList.toggle('mostrar');
+    });
+}
